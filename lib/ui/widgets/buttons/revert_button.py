d31cb33073db1088e63be18d3ca5fb02e75873b2
Marcin Goszczynski <marcin.goszczynski88@gmail.com>
2021-05-17 18:06:53 +0200

    Pylint fixes and code cleanup

--- lib/ui/widgets/buttons/revert_button.py
+++ lib/ui/widgets/buttons/revert_button.py
@@ -1,8 +1,13 @@
+"""
+Module contains RevertButton class
+"""
 from PyQt5.QtWidgets import QPushButton  # pylint: disable=no-name-in-module
 
 
 class RevertButton(QPushButton):
-
+    """
+    Implementation of revert button
+    """
     def __init__(self):
         super().__init__()
         self.setText('Przywróć')
